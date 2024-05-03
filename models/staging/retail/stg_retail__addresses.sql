7daa03cd75bfa4a4bc46c2e67c905b87e98a1501
Jayboy628 <shaunjay.brown@gmail.com>
2024-05-03 13:46:53 -0400

    add addresses

---
+++ models/staging/retail/stg_retail__addresses.sql
@@ -0,0 +1,21 @@
+with
+
+addresses as (
+
+    select * from {{ source('retail', 'addresses') }}
+
+),
+
+final as (
+
+	SELECT [AddressID] AS addressid
+	      ,[AddressLine1] AS address_line1
+	      ,[AddressLine2] AS address_line2
+	      ,[CityID] AS cityid
+	      ,[PostalCode] AS postal_code
+	      ,[ModifiedDate]AS modified_date
+	  FROM addresses
+
+)
+
+select * from final
