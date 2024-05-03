2ee01cdbfb802eb5067cd672e088aacaab11a66e
Jayboy628 <shaunjay.brown@gmail.com>
2024-05-03 13:50:29 -0400

    add department table sql

---
+++ models/staging/retail/stg_retail__product_departments.sql
@@ -0,0 +1,19 @@
+
+with
+
+product_departments as (
+
+    select * from {{ source('retail', 'productdepartments') }}
+
+),
+
+final as (
+
+	SELECT [DepartmentID]	AS departmentid
+	      ,[Name]			AS name
+	      ,[Description]	AS description
+	      ,[ModifiedDate]	AS modified_date
+	FROM product_departments
+)
+
+select * from final
