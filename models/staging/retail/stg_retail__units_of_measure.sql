b66b7d44491ca7c8199852bb37b86440df2f6a2f
Jayboy628 <shaunjay.brown@gmail.com>
2024-05-03 13:53:08 -0400

    add unit measures data not loaded

---
+++ models/staging/retail/stg_retail__units_of_measure.sql
@@ -0,0 +1,20 @@
+
+
+with
+
+units_of_measure as (
+
+    select * from {{ source('retail', 'unitsofmeasure') }}
+
+),
+
+final as (
+
+	SELECT [UnitOfMeasureID] AS units_of_measureid
+	      ,[UnitMeasureCode] AS units_measure_code	
+	      ,[Name]			 AS name
+	      ,[ModifiedDate]	 AS modified_date
+	FROM units_of_measure
+)
+
+select * from final
